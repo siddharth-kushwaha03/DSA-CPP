d05fd8b6d5a85ff42f5a84c12cade1cf0565eebe
Siddharth Kushwaha <siddhumaurya2005@gmail.com>
2026-08-23 23:18:10 +0530

    Update notebook

---  Arrays /AddressOfArray.cpp
+++  Arrays /AddressOfArray.cpp
@@ -5,6 +5,6 @@ int main() {
     int arr[]={10,90,78,56,99,98,9};
     int n =sizeof(arr)/4;
     cout<<arr[0]<<endl;
-    cout<<&arr[1]<<endl;
-    cout<<&arr[2]<<endl;
+    cout<<arr[1]<<endl;
+    cout<<arr[2]<<endl;
 }
