79f468c1da80c979b137c5378e8797b21047cbf0
Siddharth Kushwaha <siddhumaurya2005@gmail.com>
2026-08-23 23:51:34 +0530

    Update notebook

---  Arrays /PassingArrayTOfun.cpp
+++  Arrays /PassingArrayTOfun.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 using namespace std;
+void change(int x){
+    x=20;
+}
+
+int main() {
+    int x=6;
+    change(x);
+    cout<<x<<endl;
+}
+//
+
+#include <iostream>
+using namespace std;
 void change(int y[]){
     y[0]=20;
 }
